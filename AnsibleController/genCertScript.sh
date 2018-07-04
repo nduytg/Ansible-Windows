ecf89b4cff44427cb7eabf8bc1490da6b296dd04
Duy Nguyen <nduytg@gmail.com>
2018-07-04 15:02:26 +0700

    Add files via upload

---
+++ AnsibleController/genCertScript.sh
@@ -0,0 +1,14 @@
+# set the name of the local user that will have the key mapped to
+USERNAME="duynd3"
+
+cat > openssl.conf << EOL
+distinguished_name = req_distinguished_name
+[req_distinguished_name]
+[v3_req_client]
+extendedKeyUsage = serverAuth
+subjectAltName = otherName:1.3.6.1.4.1.311.20.2.3;UTF8:$USERNAME@localhost
+EOL
+
+export OPENSSL_CONF=openssl.conf
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -out cert.pem -outform PEM -keyout cert_key.pem -subj "/CN=$USERNAME" -extensions v3_req_client
+rm openssl.conf
